c5dd27feddefdab53fe1865509eea99359c65c08
Mester szakács <hornyak.akos.1020@gmail.com>
2024-12-28 12:11:35 +0100

    Kezdeti munkálatok

--- ToDoList/Form1.Designer.cs
+++ ToDoList/Form1.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            ToDoGomb = new Button();
+            ToDoCim = new Label();
+            ToDoHozzaadasa = new Label();
+            ToDoBekerese = new TextBox();
+            PontokLabel = new Label();
+            PontokMegtekinteseGomb = new Button();
+            SuspendLayout();
+
+            // 
+            // ToDoGomb
+            // 
+            ToDoGomb.Location = new Point(221, 110);
+            ToDoGomb.Name = "ToDoGomb";
+            ToDoGomb.Size = new Size(164, 27);
+            ToDoGomb.TabIndex = 2;
+            ToDoGomb.Text = "Feladat hozzáadása";
+            ToDoGomb.UseVisualStyleBackColor = true;
+            // 
+            // ToDoCim
+            // 
+            ToDoCim.AutoSize = true;
+            ToDoCim.Location = new Point(345, 21);
+            ToDoCim.Name = "ToDoCim";
+            ToDoCim.Size = new Size(85, 20);
+            ToDoCim.TabIndex = 3;
+            ToDoCim.Text = "To-Do Lista";
+            // 
+            // ToDoHozzaadasa
+            // 
+            ToDoHozzaadasa.AutoSize = true;
+            ToDoHozzaadasa.Location = new Point(48, 89);
+            ToDoHozzaadasa.Name = "ToDoHozzaadasa";
+            ToDoHozzaadasa.Size = new Size(160, 20);
+            ToDoHozzaadasa.TabIndex = 4;
+            ToDoHozzaadasa.Text = "Adj hozzá új feladatot!";
+            // 
+            // ToDoBekerese
+            // 
+            ToDoBekerese.Location = new Point(48, 128);
+            ToDoBekerese.Name = "ToDoBekerese";
+            ToDoBekerese.Size = new Size(167, 27);
+            ToDoBekerese.TabIndex = 5;
+            // 
+            // PontokLabel
+            // 
+            PontokLabel.AutoSize = true;
+            PontokLabel.Location = new Point(48, 200);
+            PontokLabel.Name = "PontokLabel";
+            PontokLabel.Size = new Size(200, 20);
+            PontokLabel.TabIndex = 6;
+            PontokLabel.Text = "Pontjaid: 0";
+            // 
+            // PontokMegtekinteseGomb
+            // 
+            PontokMegtekinteseGomb.Location = new Point(48, 230);
+            PontokMegtekinteseGomb.Name = "PontokMegtekinteseGomb";
+            PontokMegtekinteseGomb.Size = new Size(164, 27);
+            PontokMegtekinteseGomb.TabIndex = 7;
+            PontokMegtekinteseGomb.Text = "Pontok elköltése";
+            PontokMegtekinteseGomb.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(869, 619);
+            Controls.Add(PontokMegtekinteseGomb);
+            Controls.Add(PontokLabel);
+            Controls.Add(ToDoBekerese);
+            Controls.Add(ToDoHozzaadasa);
+            Controls.Add(ToDoCim);
+            Controls.Add(ToDoGomb);
+            Name = "Form1";
+            Text = "To-Do Lista";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button ToDoGomb;
+        private Label ToDoCim;
+        private Label ToDoHozzaadasa;
+        private TextBox ToDoBekerese;
+        private Label PontokLabel;
+        private Button PontokMegtekinteseGomb;
     }
 }
--- ToDoList/Form1.cs
+++ ToDoList/Form1.cs
@@ -1,10 +1,143 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
 namespace ToDoList
 {
     public partial class Form1 : Form
     {
+        private int pontok = 0; // Pontok száma
+        private List<string> feladatok = new List<string>(); // Feladatok listája
+        private FlowLayoutPanel feladatokPanel; // Panel a feladatok megjelenítésére
+
         public Form1()
         {
             InitializeComponent();
+            ToDoGomb.Click += ToDoGomb_Click;
+            PontokMegtekinteseGomb.Click += PontokMegtekinteseGomb_Click;
+
+            // Panel inicializálása
+            feladatokPanel = new FlowLayoutPanel();
+            feladatokPanel.Location = new System.Drawing.Point(48, 270);
+            feladatokPanel.Size = new System.Drawing.Size(400, 300);
+            feladatokPanel.AutoScroll = true;
+            Controls.Add(feladatokPanel);
+        }
+
+        private void ToDoGomb_Click(object sender, EventArgs e)
+        {
+            // Feladat hozzáadása
+            string ujFeladat = ToDoBekerese.Text;
+
+            if (!string.IsNullOrEmpty(ujFeladat) && ujFeladat != "Adj hozzá egy feladatot")
+            {
+                feladatok.Add(ujFeladat);
+                MessageBox.Show($"Feladat hozzáadva!\n{ujFeladat}");
+                FrissitFeladatokMegjelenitese();
+                ToDoBekerese.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Adj meg egy érvényes feladatot!");
+            }
+        }
+
+        private void FrissitFeladatokMegjelenitese()
+        {
+            feladatokPanel.Controls.Clear();
+
+            foreach (var feladat in feladatok)
+            {
+                FlowLayoutPanel sorPanel = new FlowLayoutPanel();
+                sorPanel.FlowDirection = FlowDirection.LeftToRight;
+                sorPanel.AutoSize = true;
+
+                Label feladatLabel = new Label();
+                feladatLabel.Text = feladat;
+                feladatLabel.AutoSize = true;
+
+                RadioButton keszGomb = new RadioButton();
+                keszGomb.Text = "Kész";
+                keszGomb.AutoSize = true;
+                keszGomb.CheckedChanged += (s, e) => {
+                    if (keszGomb.Checked)
+                    {
+                        Random rand = new Random();
+                        int nyertPont = rand.Next(1, 16);
+                        pontok += nyertPont;
+                        MessageBox.Show($"Feladat kész! {nyertPont} pontot kaptál. Pontjaid: {pontok}");
+                        PontokLabel.Text = $"Pontjaid: {pontok}";
+                        feladatok.Remove(feladat);
+                        FrissitFeladatokMegjelenitese();
+                    }
+                };
+
+                sorPanel.Controls.Add(feladatLabel);
+                sorPanel.Controls.Add(keszGomb);
+
+                feladatokPanel.Controls.Add(sorPanel);
+            }
+        }
+
+        private void PontokMegtekinteseGomb_Click(object sender, EventArgs e)
+        {
+            // Vásárlási ablak megnyitása
+            Form vasarlasAblak = new Form();
+            vasarlasAblak.Text = "Vásárlások";
+            vasarlasAblak.Size = new System.Drawing.Size(300, 300);
+
+            FlowLayoutPanel vasarlasPanel = new FlowLayoutPanel();
+            vasarlasPanel.Dock = DockStyle.Fill;
+            vasarlasPanel.FlowDirection = FlowDirection.TopDown;
+            vasarlasPanel.AutoScroll = true;
+            vasarlasAblak.Controls.Add(vasarlasPanel);
+
+            // Vásárlási lehetõség: Háttérszínek
+            string[] szinek = { "Kék", "Zöld", "Sárga", "Rózsaszín" };
+            System.Drawing.Color[] szinErtekek = { System.Drawing.Color.LightBlue, System.Drawing.Color.LightGreen, System.Drawing.Color.LightYellow, System.Drawing.Color.LightPink };
+
+            for (int i = 0; i < szinek.Length; i++)
+            {
+                Button szinGomb = new Button();
+                szinGomb.Text = $"{szinek[i]} háttér (50 pont)";
+                int szinIndex = i; // Rögzítjük az aktuális indexet a lambda kifejezéshez
+                szinGomb.Click += (s, e) => {
+                    if (pontok >= 50)
+                    {
+                        pontok -= 50;
+                        this.BackColor = szinErtekek[szinIndex];
+                        MessageBox.Show($"A háttérszín megváltozott: {szinek[szinIndex]}! Pontjaid: {pontok}");
+                        PontokLabel.Text = $"Pontjaid: {pontok}";
+                        vasarlasAblak.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Nincs elég pontod ehhez a vásárláshoz!");
+                    }
+                };
+                vasarlasPanel.Controls.Add(szinGomb);
+            }
+
+            // Vásárlási lehetõség 2: Speciális funkció aktiválása
+            Button vasarlas2 = new Button();
+            vasarlas2.Text = "Speciális funkció aktiválása (100 pont)";
+            vasarlas2.Click += (s, e) => {
+                if (pontok >= 100)
+                {
+                    pontok -= 100;
+                    MessageBox.Show("Speciális funkció aktiválva!");
+                    PontokLabel.Text = $"Pontjaid: {pontok}";
+                    vasarlasAblak.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Nincs elég pontod ehhez a vásárláshoz!");
+                }
+            };
+
+            vasarlasPanel.Controls.Add(vasarlas2);
+
+            vasarlasAblak.ShowDialog();
         }
     }
 }
